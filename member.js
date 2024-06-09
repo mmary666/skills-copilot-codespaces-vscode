1099ae79310d3a19f2a7e6d2cf54379caf04da1a
mmary666 <35154119+mmary666@users.noreply.github.com>
2024-06-09 19:48:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMember() {
+    var skills = [
+      {
+        name: 'HTML',
+        level: 'Advanced',
+        years: 5
+      },
+      {
+        name: 'CSS',
+        level: 'Advanced',
+        years: 5
+      },
+      {
+        name: 'JavaScript',
+        level: 'Intermediate',
+        years: 3
+      }
+    ];
+    return skills;
+  }
